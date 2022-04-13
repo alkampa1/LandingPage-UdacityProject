734e703038c5472aff7e989ff6273a815d42b37b
Alba K <rubykrm@yahoo.com>
2022-04-12 22:56:01 -0500

    Second commit

--- js/app.js
+++ js/app.js
@@ -40,31 +40,22 @@ navBuilder = () => {
 
     // creates the nav by settings the attributes, adding the class and appending the anchor tag to the li
     anchor.setAttribute("href", "#" + link);
-    anchor.setAttribute("id", "secNum" + [i + 1]);
     anchor.className += "menu__link";
     anchor.innerHTML = name;
     li.appendChild(anchor);
 
     // Appends everything to the ul tag
     navList.appendChild(li);
-
-    // Adds scroll to anchor ID
-    document
-      .getElementById("secNum" + [i + 1])
-      .addEventListener("click", () => {
-        console.log("heyy!");
-        ScrollToAchor(i + 1);
-      });
   }
 };
 
 /**
  * @desc ddds an 'active' class to the sections when near top of viewport
  */
-makeActive = () => {
+activeSection = () => {
   for (let section of sections) {
     let box = section.getBoundingClientRect();
-    if (box.top >= 0) {
+    if (box.top <= 150 && box.bottom >= 150) {
       section.classList.add("your-active-class");
     } else {
       section.classList.remove("your-active-class");
@@ -73,31 +64,56 @@ makeActive = () => {
 };
 
 /**
- * @desc add class 'active' to nav menu
+ * @desc Scroll to anchor ID using scrollTO event
  */
-activeNav = () => {
-  let links = navList.querySelectorAll("li");
-  for (let link of links) {
-    link.addEventListener("click", () => {
-      if (link.className === "active") {
-        link.classList.remove("active");
-      } else {
-        link.classList.add("active");
-      }
+ScrollToAchor = () => {
+  let sectionMenu = document.querySelectorAll(".menu_link");
+  for (sect of sectionMenu) {
+    sect.addEventListener("click", function (e) {
+      e.preventDefault();
+      sect.scrollIntoView({
+        behavior: "smooth",
+      });
     });
   }
 };
 
 /**
- * @desc Scroll to anchor ID using scrollTO event
- * @param {num} the number in the id of the active nav
+ * End Main Functions
+ * Begin Events and function calls
+ *
  */
-ScrollToAchor = (num) => {
-  let sectionMenu = document.querySelector("#secNum" + num);
-  window.scrollTo({
-    top: sectionMenu,
-    behavior: "smooth",
-  });
+
+// Build Navigation
+navBuilder();
+
+// Scroll to Anchor
+ScrollToAchor();
+
+// Section Active State
+document.addEventListener("scroll", activeSection);
+
+/**
+ *  Additional features
+ *
+ */
+
+/**
+ * @desc add class 'active' to nav menu
+ */
+activeNav = () => {
+  let links = navList.querySelectorAll("li");
+  for (let link of links) {
+    link.addEventListener("click", function (e) {
+      //e.preventDefault(); --- Removing because preventDefault() stops the scroll to section event. Using stopImmediatePropagation() instead.
+      e.stopImmediatePropagation();
+      let prev = document.getElementsByClassName("activeNv");
+      if (prev && prev[0]) {
+        prev[0].classList.remove("activeNv");
+      }
+      this.classList.add("activeNv");
+    });
+  }
 };
 
 /**
@@ -124,32 +140,23 @@ collSections = () => {
  *
  */
 backToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+  bkTopBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
 };
 
-/**
- * End Main Functions
- * Begin Events and function calls
- *
- */
+// Active top navigation
+activeNav();
 
 // scroll to top button
-bkTopBtn.addEventListener("click", backToTop);
+backToTop();
 
 // Collapsible sections
 collSections();
-
-// Active top navigation
-document.addEventListener("click", activeNav);
-
-// Build menu
-navBuilder();
-
-// Set sections as active
-document.addEventListener("scroll", makeActive);
 
 /**
  * Ends Events and function calls
